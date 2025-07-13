a83a41f3e93bd80c7953c0659702159215b1e120
ZhuXi <xiaofei6626@126.com>
2025-07-13 12:23:03 +0800

    feat(security): 添加 XSS防护过滤器
    - 在 pom.xml 中添加 jsoup 依赖- 新增 SecurityConfig 配置类，注册 XSSFilter
    - 实现 XSSFilter 类，对请求参数进行 XSS 清洗
    - 使用 Jsoup 进行 HTML 内容的安全清理

---
+++ parent/service/src/main/java/com/zhuxi/Filter/XSSFilter.java
@@ -0,0 +1,75 @@
+package com.zhuxi.Filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class XSSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+         filterChain.doFilter(new XssSafeRequestWrapper((HttpServletRequest) servletRequest),servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
+
+   @Slf4j
+   class XssSafeRequestWrapper extends HttpServletRequestWrapper{
+        public XssSafeRequestWrapper(HttpServletRequest request) {
+            super(request);
+        }
+
+        @Override
+      public String getParameter(String name){
+            return cleanXSS(super.getParameter(name));
+        }
+
+        @Override
+        public String[] getParameterValues(String values){
+            String[] parameterValues = super.getParameterValues(values);
+            if(values == null){
+                return null;
+            }
+            return Arrays.stream(parameterValues).map(this::cleanXSS).toArray(String[]::new);
+        }
+
+
+        private String cleanXSS(String value){
+            if (value == null){
+                return null;
+            }
+
+            String cleaned = Jsoup.clean(
+                    value,
+                    Safelist.basicWithImages()
+                            .addTags("div","span")
+                            .addAttributes("a","target","rel")
+                            .addEnforcedAttribute("a","rel","noopener")
+                            .addProtocols("a","hef","http","https")
+                            .addProtocols("img","src","http","https")
+            );
+
+            cleaned = cleaned.replaceAll("[\u0000-\u001F]", "");
+
+            if(value.equals(cleaned)){
+                log.warn("XSS is triggered: before-Value{}  after-Value{}", value, cleaned);
+            }
+            return cleaned;
+        }
+
+}
