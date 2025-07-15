23739905e874e140a3e217d7af39cd2f0a4e6758
ZhuXi <xiaofei6626@126.com>
2025-07-15 17:29:55 +0800

    优化拦截路径 构建redis基础模块

--- parent/service/src/main/java/com/zhuxi/config/RedisConfig.java
+++ parent/service/src/main/java/com/zhuxi/config/RedisConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.zhuxi.config;
 
 import org.springframework.context.annotation.Bean;
@@ -25,3 +26,4 @@ public class RedisConfig {
         return RedisTemplate;
     }
 }
+*/
--- parent/service/src/test/java/com/zhuxi/service/ServiceApplicationTests.java
+++ parent/service/src/test/java/com/zhuxi/service/ServiceApplicationTests.java
@@ -2,18 +2,27 @@ package com.zhuxi.service;
 
 import cn.hutool.core.util.IdUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 
 @SpringBootTest
 class ServiceApplicationTests {
 
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    public ServiceApplicationTests(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     @Test
     void contextLoads() {
 
-        long l = IdUtil.getSnowflake(1, 1).nextId();
-        String format = String.format("%s%d", "OD", l);
-        System.out.println(format);
+        ValueOperations<String, String> ValueOperations = stringRedisTemplate.opsForValue();
+        ValueOperations.set("test", IdUtil.simpleUUID());
     }
 
 
